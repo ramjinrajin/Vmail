0b3a7437ceb8478d013a91906270c6a66697a8e4
ramjin.rajin@hotmail.com <ramjin.rajin@hotmail.com>
2018-03-27 00:18:40 +0530

    Db Script updated

---
+++ VmailApiAuth/App_Data/Script/RemoteUser.sql
@@ -0,0 +1,48 @@
+﻿ 
+ 
+Create Table VmailUser
+(
+Id int Identity(1,1),
+Userid int NOT NULL,
+AuthCount int default 0 NOT NULL,
+HITCount int default 0 NOT NULL,
+IsAuthenticated int default 0
+)
+GO
+-------------------------------------------------
+CREATE Proc SpVmailUser
+(
+	@UserId int
+)
+AS
+BEGIN
+ 
+Declare @HITCount int=0;
+Declare @AuthCount int=0;
+SET @HITCount=(SELECT HITCount FROM VmailUser Where Userid=@UserId)
+SET @AuthCount=(SELECT AuthCount FROM VmailUser Where Userid=@UserId)
+
+
+
+  SET @HITCount=(SELECT HITCount FROM VmailUser Where Userid=@UserId)
+  IF(@HITCount=@AuthCount)
+    Update VmailUser SET IsAuthenticated=1 Where Userid=@UserId
+	ELSE 
+		  Update VmailUser SET HITCount=@HITCount+1 Where Userid=@UserId
+
+
+  SET @HITCount=(SELECT HITCount FROM VmailUser Where Userid=@UserId)
+
+
+
+  SELECT @HITCount AS RESPONSE
+
+EnD
+Go
+
+--------------------------------------------------
+
+INSERT INTO VmailUser (Userid, AuthCount, HITCount, IsAuthenticated)
+	VALUES (1, 0, 0, 0)
+
+EXEC SpVmailUser 1
